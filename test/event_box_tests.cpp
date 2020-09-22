c5cfb9a64bffd0c118589075f403b05fcb90c2cd
Aymeric Pellé <aympelle@gmail.com>
2020-09-22 13:17:04 +0200

    v0.1.0:EVNT#2: Add unit tests for event_box.

--- test/event_box_tests.cpp
+++ test/event_box_tests.cpp
@@ -25,6 +25,48 @@ TEST(event_box_tests, test_connection)
     event_manager.connect(event_box);
     event_manager.emit(int_event{ 5 });
     ASSERT_EQ(value, 0);
+
+    event_box.emit_received_events();
+    ASSERT_EQ(value, 5);
+}
+
+TEST(event_box_tests, test_connection_2)
+{
+    evnt::event_manager event_manager;
+
+    evnt::event_box event_box;
+    event_manager.connect(event_box);
+    event_manager.emit(int_event{ 5 });
+
+    int value = 0;
+    event_box.connect<int_event>([&value](int_event& event)
+    {
+        value = event.value;
+    });
+    ASSERT_EQ(value, 0);
+
+    event_box.emit_received_events();
+    ASSERT_EQ(value, 5);
+}
+
+TEST(event_box_tests, test_auto_deconnection)
+{
+    evnt::event_manager event_manager;
+    int value = 0;
+
+    {
+        evnt::event_box event_box;
+        event_box.connect<int_event>([&value](int_event& event)
+        {
+            value = event.value;
+        });
+        event_manager.connect(event_box);
+        event_manager.emit(int_event{ 5 });
+        event_box.emit_received_events();
+        ASSERT_EQ(value, 5);
+    }
+
+    event_manager.emit(int_event{ 8 });
 }
 
 int main(int argc, char** argv)
